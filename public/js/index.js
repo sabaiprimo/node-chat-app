8db643476d138783ff419186c81391a30014a21a
Sabai <sabaiprimo@gmail.com>
2019-02-18 10:32:07 +0700

    Add newMessage and createMessage event

---
+++ public/js/index.js
@@ -0,0 +1,22 @@
+var socket = io();
+        
+socket.on('connect', function () {
+    console.log('Connected to Server');
+
+    socket.emit('createMessage',{
+        from: 'sabai',
+        text: 'Hi jen!'
+    });
+});
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message){
+    console.log('new message', message);
+});
+
+socket.on('newEmail', function (email){
+    console.log('New email', email);
+});
